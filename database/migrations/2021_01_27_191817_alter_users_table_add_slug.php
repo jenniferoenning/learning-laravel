b8cf9d1f0910434d90a224c787dc6aa7b9d9e5cc
jenniferoenning <oenningstangjennifer@gmail.com>
2021-01-29 13:57:46 -0300

    commenting migrate alter slug

--- database/migrations/2021_01_27_191817_alter_users_table_add_slug.php
+++ database/migrations/2021_01_27_191817_alter_users_table_add_slug.php
@@ -13,9 +13,9 @@ class AlterUsersTableAddSlug extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('slug')->nullable();
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->string('slug')->nullable();
+        // });
     }
 
     /**
@@ -25,8 +25,8 @@ class AlterUsersTableAddSlug extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn();
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->dropColumn();
+        // });
     }
 }
